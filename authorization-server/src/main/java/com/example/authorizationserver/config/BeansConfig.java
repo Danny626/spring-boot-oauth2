4626eaedf491aef2254d44954f3f1bf596199b65
Danny <live626@gmail.com>
2023-12-08 17:21:53 -0400

    origin ip changed

--- authorization-server/src/main/java/com/example/authorizationserver/config/BeansConfig.java
+++ authorization-server/src/main/java/com/example/authorizationserver/config/BeansConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class BeansConfig {
     @Bean
@@ -22,7 +24,8 @@ public class BeansConfig {
         cors.addAllowedHeader("*");
         cors.addAllowedMethod("*");
         cors.setAllowCredentials(true);
-        cors.addAllowedOrigin("http://localhost:4200");
+        // cors.setAllowedOrigins(List.of("http://127.0.0.1:4200", "http://localhost:4200"));
+        cors.addAllowedOrigin("http://127.0.0.1:4200");
         source.registerCorsConfiguration("/**", cors);
 
         return source;
